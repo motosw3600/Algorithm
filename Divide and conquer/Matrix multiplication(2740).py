d08f8b0d535fc83b801d7d96d48a903ac44d8200
motosw3600 <motosw3600@gmail.com>
2021-05-06 00:35:16 +0900

    Matrix multiplication change

--- Divide and conquer/Matrix multiplication(2740).py
+++ Divide and conquer/Matrix multiplication(2740).py
@@ -1,26 +1,16 @@
 N, M = map(int, input().split())
-a_list = []
-b_list = []
-for _ in range(N):
-    a_list.append(list(map(int, input().split())))
-
+a_list = [list(map(int, input().split())) for x in range(N)]
 O, K = map(int, input().split())
-for _ in range(O):
-    b_list.append(list(map(int, input().split())))
+b_list = [list(map(int, input().split())) for x in range(O)]
 
-c_list = [[0 for x in range(N)] for y in range(K)]
-
-def matrix_num(a, b):
+def matrix_num(x, y):
     n = 0
     for i in range(M):
-        for j in range(O):
-            n += (a_list[i][j] + b_list[j][i])
+        n += (a_list[x][i] * b_list[i][y])
     return n
 
-for i in range(K):
-    for j in range(N):
-        c_list[i][j] = 
+for i in range(N):
+    for j in range(K):
+        print(matrix_num(i, j), end=" ")
+    print()
 
-a_list[0][0], a_list[0][1], b_list[0][0], b_list[1][0]
-a_list[0][0], a_list[0][1], b_list[1][0], b_list[1][1]
-a_list[0][0], a_list[0][1], b_list[2][0], b_list[1][2]
