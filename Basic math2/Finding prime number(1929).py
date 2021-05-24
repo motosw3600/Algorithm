19f2d1478b703c047dd5b966cd9c915213ba1202
motosw3600 <motosw3600@gmail.com>
2021-05-24 23:52:55 +0900

    Basic math2/Finding prime number(1929).py change

--- Basic math2/Finding prime number(1929).py
+++ Basic math2/Finding prime number(1929).py
@@ -1,13 +1,15 @@
-M, N = map(int, input().split())
+# M, N = map(int, input().split())
 
-def prime(num):
-    if num == 1:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
+# def prime(num):
+#     if num == 1:
+#         return False
+#     for i in range(2, int(num ** 0.5) + 1):
+#         if num % i == 0:
+#             return False
+#     return True
+#
+# for i in range(M, N + 1):
+#     if prime(i):
+#         print(i)
 
-for i in range(M, N + 1):
-    if prime(i):
-        print(i)
+print(int(9 ** 0.5))
