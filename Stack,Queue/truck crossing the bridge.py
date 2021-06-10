89bf22c51547ca120b25d11a03e16e63f4829634
motosw3600 <motosw3600@gmail.com>
2021-06-10 23:14:18 +0900

    Stack,Queue/truck crossing the bridge.py change

--- Stack,Queue/truck crossing the bridge.py
+++ Stack,Queue/truck crossing the bridge.py
@@ -1,39 +1,39 @@
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    bridge = deque(0 for _ in range(bridge_length))
-    total_weight = 0
-    step = 0
-    truck_weights.reverse()
+    time = 0
+    q = [0] * bridge_length
 
-    while truck_weights:
-        print(total_weight, bridge)
-        total_weight -= bridge.popleft()
-        if total_weight + truck_weights[-1] > weight:
-            bridge.append(0)
-        else:
-            truck = truck_weights.pop()
-            bridge.append(truck)
-            total_weight += truck
-        step += 1
+    while q:
+        time += 1
+        q.pop(0)
+        if truck_weights:
+            if sum(q) + truck_weights[0] <= weight:
+                q.append(truck_weights.pop(0))
+            else:
+                q.append(0)
 
-    step += bridge_length
-
-    return step
-
-# def solution(bridge_length, weight, truck_weights):
-#     time = 0
-#     q = [0] * bridge_length
-#
-#     while q:
-#         time += 1
-#         q.pop(0)
-#         if truck_weights:
-#             if sum(q) + truck_weights[0] <= weight:
-#                 q.append(truck_weights.pop(0))
-#             else:
-#                 q.append(0)
-#
-#     return time
+    return time
 
 solution(2, 10, [7,4,5,6])
+
+# def solution(bridge_length, weight, truck_weights):
+#     bridge = deque(0 for _ in range(bridge_length))
+#     total_weight = 0
+#     step = 0
+#     truck_weights.reverse()
+#
+#     while truck_weights:
+#         print(total_weight, bridge)
+#         total_weight -= bridge.popleft()
+#         if total_weight + truck_weights[-1] > weight:
+#             bridge.append(0)
+#         else:
+#             truck = truck_weights.pop()
+#             bridge.append(truck)
+#             total_weight += truck
+#         step += 1
+#
+#     step += bridge_length
+#
+#     return step
