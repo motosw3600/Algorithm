225763239e092d7d0e8e9937c0a6534856379453
motosw3600 <motosw3600@gmail.com>
2021-06-26 21:31:29 +0900

    Stack,Queue/printer.py change

--- Stack,Queue/printer.py
+++ Stack,Queue/printer.py
@@ -1,19 +1,16 @@
 from collections import deque
 
+
 def solution(priorities, location):
-    queue = deque()
-    for idx, val in enumerate(priorities):
-        queue.append([val, idx])
     answer = 0
-    cnt = 0
-    while queue:
-        val = queue.popleft()
-        if len(queue) > 0 and val[0] < max([x[0] for x in queue]):
-            queue.append(val)
+    dq = deque([[val, idx] for idx, val in enumerate(priorities)])
+    while dq:
+        val = dq.popleft()
+        if len(dq) > 0 and val[0] < max([x[0] for x in dq]):
+            dq.append(val)
         else:
-            cnt += 1
+            answer += 1
             if val[1] == location:
-                answer = cnt
                 break
 
     return answer
