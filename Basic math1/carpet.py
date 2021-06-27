0282903c6ae5caaa80cd9a0923f900335dee9d13
motosw3600 <motosw3600@gmail.com>
2021-06-27 17:35:39 +0900

    Basic math1/carpet.py check1

--- Basic math1/carpet.py
+++ Basic math1/carpet.py
@@ -1,21 +1,11 @@
 def solution(brown, yellow):
     answer = []
     sum = brown + yellow
-    n_list = [[1, yellow]]
-    i = 2
-    while i < yellow // 2 + 1:
-        if yellow % i == 0:
-            n_list.append([i, yellow // i])
-        i += 1
 
-    for val in n_list:
-        a = val[0] + 2
-        b = val[1] + 2
-        if a * b == sum:
-            if b > a:
-                a, b = b, a
-            answer.append(a)
-            answer.append(b)
+    for i in range(3, sum // 2 + 1):
+        j = sum // i
+        if (i - 2) * (j - 2) == yellow:
+            answer = [j, i]
             break
 
     return answer
