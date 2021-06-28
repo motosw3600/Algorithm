f4de021f3f9799d812c1b966cd99bb0954ff85fc
motosw3600 <motosw3600@gmail.com>
2021-06-28 16:36:49 +0900

    DFS and BFS/Target number.py change

--- DFS and BFS/Target number.py
+++ DFS and BFS/Target number.py
@@ -1,17 +1,34 @@
-cnt = 0
+from collections import deque
+answer = 0
 def solution(numbers, target):
-    dfs(numbers, target, 0)
-    return cnt
+    return bfs(numbers, target)
+    # dfs(numbers, target, 0)
+    # return answer
+
 
 def dfs(numbers, target, num):
-    global cnt
-
+    global answer
     if num == len(numbers) and target == 0:
-        cnt += 1
+        answer += 1
         return
 
-    if num == len(numbers):
+    elif num == len(numbers):
         return
 
-    dfs(numbers, target - numbers[num], num + 1)
     dfs(numbers, target + numbers[num], num + 1)
+    dfs(numbers, target - numbers[num], num + 1)
+
+
+def bfs(numbers, target):
+    answer = 0
+    queue = deque([[0, 0]])
+    length = len(numbers)
+    while queue:
+        val = queue.popleft()
+        if val[0] < length:
+            queue.append([val[0] + 1, val[1] + numbers[val[0]]])
+            queue.append([val[0] + 1, val[1] - numbers[val[0]]])
+        elif val[1] == target:
+            answer += 1
+
+    return answer
