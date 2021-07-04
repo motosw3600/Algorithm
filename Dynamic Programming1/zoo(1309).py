1d530ffba073bb70e27dad6379e2719b6a027746
motosw3600 <motosw3600@gmail.com>
2021-07-04 13:17:41 +0900

    Dynamic Programming1/zoo(1309).py check1

--- Dynamic Programming1/zoo(1309).py
+++ Dynamic Programming1/zoo(1309).py
@@ -1,8 +1,9 @@
 n = int(input())
+# 경우의 수 왼쪽에만 있을 때 오른쪽에만 있을 때 아무것도 없을 때
 s = [[0] * 3 for i in range(100001)]
 for i in range(3):
     s[1][i] = 1
-for i in range(2, 100001):
+for i in range(2, n+1):
     s[i][0] = s[i - 1][1] + s[i - 1][2] % 9901
     s[i][1] = s[i - 1][0] + s[i - 1][2] % 9901
     s[i][2] = s[i - 1][0] + s[i - 1][1] + s[i - 1][2] % 9901
