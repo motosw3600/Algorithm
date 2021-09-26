3e29c47d33ce45aa460c560a9fd643bb757e0c16
motosw3600 <motosw3600@gmail.com>
2021-09-27 00:00:57 +0900

    fix: remove Mark

--- Recursion/star shooting(2447).py
+++ Recursion/star shooting(2447).py
@@ -1,22 +1,3 @@
-# n = int(input())
-# star = [['*' for i in range(n)] for j in range(n)]
-#
-# cnt = 0
-# v = n
-# while v != 1:
-#     v //= 3
-#     cnt += 1
-#
-# for s in range(cnt):
-#     idx = [i for i in range(n) if ((i // (3 ** s))) % 3 == 1]
-#
-#     for i in idx:
-#         for j in idx:
-#             star[i][j] = " "
-#
-# for i in range(n):
-#     print("".join(star[i]))
-
 def get_stars(n):
     matrix = []
     for i in range(3 * len(n)):
